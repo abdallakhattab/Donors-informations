2637488a141a05750f7132533e6938ab664c3e7e
abdallakhattab <khattababdalla@yahoo.com>
2021-05-04 13:29:02 +0300

    finished the exam

---
+++ js/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let arrofobj = [];
+
+function getRandomAge(max, min) {
+
+  return Math.floor(Math.random() * (max - min) + 1);
+}
+function Donars(donarName, amount) {
+
+  this.donarName = donarName;
+  this.randomAge = getRandomAge(30, 18);
+  this.amount = amount;
+  arrofobj.push(this);
+
+}
+// Donars.prototype.bton = function(){
+//   let btn = document.getElementById('btn');
+//   btn.addEventListener('click',this.addingDonar );
+// };
+// Donars.prototype.addingDonar = function(){
+//   this.bton();
+//   let donName = document.getElementById('donorName').value;
+//   let donamount = document.getElementById('amount').value;
+//   let newDonar = new Donars(donName , donamount);
+// };
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', addingDonar);
+function addingDonar () {
+
+  let donName = document.getElementById('donorName').value;
+  //console.log(donName);
+  let donamount = document.getElementById('amount').value;
+  console.log(donamount);
+  event.preventDefault();
+  let newDonar = new Donars(donName, donamount);
+  let lsarr = JSON.stringify(arrofobj);
+  localStorage.setItem('arrofobj', lsarr);
+  //console.log(newDonar);
+}
+console.log(arrofobj);
+function rendertable() {
+  for( let i =0 ; i <= arrofobj.length ; i++){
+    let norArrOfObj = localStorage.getItem('arrofobj');
+    let norArrOfObj2 = JSON.parse(norArrOfObj);
+    let table = document.getElementById('table');
+    let trEl1 = document.createElement('tr');
+    table.appendChild(trEl1);
+    let tdEl = document.createElement('td');
+    trEl1.appendChild(tdEl);
+    tdEl.textContent = norArrOfObj2;
+    console.log(norArrOfObj2);
+  }
+}
+rendertable();
